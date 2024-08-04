e08829bc0a0af19b373e65bd58e03852d6b1ae2a
Daja <dajasvec@gmail.com>
2024-08-04 12:13:45 +0200

    Adding tests

--- hand.py
+++ hand.py
@@ -1,27 +1,53 @@
 from card import Card
 from typing import List
+from collections import Counter
 
-class Hand:
+class PokerHand:
     def __init__(self, cards: List[Card]) -> None:
+        if len(cards) != 5:
+            raise ValueError("Poker hand has to contain 5 cards!")
         self.cards = cards
 
     def __str__(self) -> str:
         return " ".join([str(card) for card in self.cards])
     
-    def is_flush(self):
+    def flush(self):
         suits = [card.get_suit() for card in self.cards]
         return len(set(suits)) == 1
     
-    def is_straight(self):
+    def straight(self):
         ranks = sorted([card.get_rank() for card in self.cards])
         for i in range(len(ranks)-1):
             if ranks[i]+1 != ranks[i+1]:
                 return False
         return True
-
     
-# c1 = Card("2", "H")
-# c2= Card("3", "S")
-# c3= Card("4", "D")
-# h = Hand([c3,c2,c2])
-# print(h.is_straight())
+    @staticmethod
+    def of_a_kind(cards: List[Card], number: int) -> bool:
+        rank_counter = Counter([card.get_rank() for card in cards])
+        for rank in rank_counter:
+            if rank_counter[rank] == number:
+                return True
+        return False
+    
+    def four_of_a_kind(self) -> bool:
+        return self.of_a_kind(self.cards, 4)
+    
+    def three_of_a_kind(self) -> bool:
+        return self.of_a_kind(self.cards, 3)
+    
+    def two_of_a_kind(self) -> bool:
+        return self.of_a_kind(self.cards, 2)
+
+    def full_house(self) -> bool:
+        return self.three_of_a_kind() and self.two_of_a_kind()
+
+    def two_pair(self) -> bool:
+        rank_counter = Counter([card.get_rank() for card in self.cards])
+        for _, count in rank_counter.most_common(2):
+            if count != 2:
+                return False
+        return True
+
+    def high_card(self) -> int:
+        return max(self.cards, key=lambda x: x.get_rank()).get_rank()
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -1,12 +1,61 @@
 import unittest
-from hand import Hand
+from hand import PokerHand
 from card import Card
 
-class TestHand(unittest.TestCase):
-    def test_str(self):
-        h = Hand([
-            Card("2","D"),
-            Card("3","S"),
-            Card("5","H"),
+class TestPokerHand(unittest.TestCase):
+    def setUp(self) -> None:
+        self.straight_flush = PokerHand([Card(str(x), "D") for x in range(2,7)])
+        self.poker = PokerHand([
+            Card("A", "D"),
+            Card("A", "S"),
+            Card("A", "H"),
+            Card("A", "C"),
+            Card("K","D")
         ])
-        self.assertIsInstance(str(h), str)
+        self.full_house = PokerHand([
+            Card("A", "D"),
+            Card("A", "S"),
+            Card("A", "H"),
+            Card("K", "C"),
+            Card("K","D")
+        ])
+        self.two_pair = PokerHand([
+            Card("A", "D"),
+            Card("A", "S"),
+            Card("2", "H"),
+            Card("K", "C"),
+            Card("K","D")
+        ])
+        return super().setUp()
+    def test_less_than_5_cards(self):
+        with self.assertRaises(ValueError) as context:
+            hand = PokerHand([Card("2","D") for _ in range(4)])
+
+    def test_more_than_5_cards(self):
+        with self.assertRaises(ValueError) as context:
+            hand = PokerHand([Card("2","D") for _ in range(6)])
+
+    def test_str(self):
+        hand = PokerHand([Card("2","D") for _ in range(5)])
+        self.assertIsInstance(str(hand), str)
+
+    def test_straight(self):
+        self.assertTrue(self.straight_flush.straight()) 
+
+    def test_flush(self):
+        self.assertTrue(self.straight_flush.flush())
+
+    def test_four_of_a_kind(self):
+        self.assertTrue(self.poker.four_of_a_kind())
+
+    def test_three_of_a_kind(self):
+        self.assertTrue(self.full_house.three_of_a_kind())
+
+    def test_two_of_a_kind(self):
+        self.assertTrue(self.full_house.two_of_a_kind())
+
+    def test_two_pair(self):
+        self.assertTrue(self.two_pair.two_pair())
+
+    def test_high_card(self):
+        self.assertEqual(self.two_pair.high_card(), 12)
